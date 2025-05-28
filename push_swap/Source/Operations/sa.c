3bf75dcb12ce257009c27889e2887de2efcf5d7c
Raja Vikraman <rvikrama@u90z02s02.42kl.edu.my>
2025-05-28 15:47:40 +0800

    Fix :
    Description :	• Fix the norminette errors for the operation functions.
    
    		• And The code still works fine, the issues was that
    		  the indesnts were just spaces and not tabs.

--- push_swap/Source/Operations/sa.c
+++ push_swap/Source/Operations/sa.c
@@ -1,59 +1,50 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   sa.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rvikrama <rvikrama@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rvikrama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/30 10:48:18 by rvikrama          #+#    #+#             */
-/*   Updated: 2025/05/28 13:45:11 by rvikrama         ###   ########.fr       */
+/*   Created: 2025/05/28 15:37:02 by rvikrama          #+#    #+#             */
+/*   Updated: 2025/05/28 15:37:16 by rvikrama         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "../../Includes/push_swap.h"
 
+void	sa(t_push_swap *data);
+void	print_stack_a(t_stack *pile, char name);
 
-void    sa(t_push_swap *data);
-void    print_stack_a(t_stack *pile, char name);
-
-void    sa(t_push_swap *data)
+void	sa(t_push_swap *data)
 {
-    int temp;
+	int	temp;
 
-    if(data->a.top < 1)
-        return ;
-    temp = data->a.numbers[data->a.top];
-    //printf("%d\n", temp);
-    data->a.numbers[data->a.top] = data->a.numbers[data->a.top - 1];
-    data->a.numbers [data->a.top -1] = temp;
-    ft_putstr_fd("sa\n", 1);
+	if (data->a.top < 1)
+		return ;
+	temp = data->a.numbers[data->a.top];
+	data->a.numbers[data->a.top] = data->a.numbers[data->a.top - 1];
+	data->a.numbers[data->a.top - 1] = temp;
+	ft_putstr_fd("sa\n", 1);
 }
 
-
-// This function help in printing the numbers and output onto the 
-// terminal.
-
-void    print_stack_a(t_stack *pile, char name)
+void	print_stack_a(t_stack *pile, char name)
 {
-    int i;
+	int	i;
 
-    ft_putstr_fd("Stack ", 1);
-    ft_putchar_fd(name , 1);
-    ft_putstr_fd(": ", 1);
-
-    if (pile->top == -1)
-    {
-        ft_putstr_fd("(empty)\n", 1);
-        return ;
-    }
-
-    i = 0;
-    while (i <= pile->top)
-    {
-        ft_putnbr_fd(pile->numbers[i], 1);
-        ft_putchar_fd(' ', 1);
-        i++;
-    }
-    ft_putchar_fd('\n', 1);
+	ft_putstr_fd("Stack ", 1);
+	ft_putchar_fd(name, 1);
+	ft_putstr_fd(": ", 1);
+	if (pile->top == -1)
+	{
+		ft_putstr_fd("(empty)\n", 1);
+		return ;
+	}
+	i = 0;
+	while (i <= pile->top)
+	{
+		ft_putnbr_fd(pile->numbers[i], 1);
+		ft_putchar_fd(' ', 1);
+		i++;
+	}
+	ft_putchar_fd('\n', 1);
 }
-
--- push_swap/Source/Operations/sb.c
+++ push_swap/Source/Operations/sb.c
@@ -1,53 +1,50 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   sb.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rvikrama <rvikrama@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rvikrama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/30 10:48:27 by rvikrama          #+#    #+#             */
-/*   Updated: 2025/05/28 13:45:16 by rvikrama         ###   ########.fr       */
+/*   Created: 2025/05/28 15:37:57 by rvikrama          #+#    #+#             */
+/*   Updated: 2025/05/28 15:38:01 by rvikrama         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "../../Includes/push_swap.h"
 
+void	sb(t_push_swap *data);
+void	print_stack_b(t_stack *pile, char name);
 
-void sb(t_push_swap *data);
-void    print_stack_b(t_stack *pile, char name);
-
-void    sb(t_push_swap *data)
+void	sb(t_push_swap *data)
 {
-    int temp;
+	int	temp;
 
-    if(data->b.top < 1)
-        return ;
-    temp = data->b.numbers[data->b.top];
-    data->b.numbers[data->b.top] = data->b.numbers[data->b.top - 1];
-    data->b.numbers [data->b.top -1] = temp;
-    ft_putstr_fd("sb\n", 1);
+	if (data->b.top < 1)
+		return ;
+	temp = data->b.numbers[data->b.top];
+	data->b.numbers[data->b.top] = data->b.numbers[data->b.top - 1];
+	data->b.numbers[data->b.top - 1] = temp;
+	ft_putstr_fd("sb\n", 1);
 }
 
-void    print_stack_b(t_stack *pile , char name)
+void	print_stack_b(t_stack *pile, char name)
 {
-    int i;
+	int	i;
 
-    ft_putstr_fd("Stack ", 1);
-    ft_putchar_fd(name, 1);
-    ft_putstr_fd(": ", 1);
-
-    if (pile->top == -1)
-    {
-        ft_putstr_fd("(empty)\n", 1);
-        return ;
-    }
-
-    i = 0;
-    while (i <= pile->top)
-    {
-        ft_putnbr_fd(pile->numbers[i], 1);
-        ft_putchar_fd(' ', 1);
-        i++;
-    }
-    ft_putchar_fd('\n', 1);
+	ft_putstr_fd("Stack ", 1);
+	ft_putchar_fd(name, 1);
+	ft_putstr_fd(": ", 1);
+	if (pile->top == -1)
+	{
+		ft_putstr_fd("(empty)\n", 1);
+		return ;
+	}
+	i = 0;
+	while (i <= pile->top)
+	{
+		ft_putnbr_fd(pile->numbers[i], 1);
+		ft_putchar_fd(' ', 1);
+		i++;
+	}
+	ft_putchar_fd('\n', 1);
 }
